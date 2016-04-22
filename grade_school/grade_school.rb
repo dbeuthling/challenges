aef63aad3a7dd898a4391f493044ff0b03dc0954
Dan Beuthling <danbeuthling@gmail.com>
2016-04-21 23:25:33 -0500

    refactored grade_school and started circular_buffer challenge

--- grade_school/grade_school.rb
+++ grade_school/grade_school.rb
@@ -1,34 +1,18 @@
 class School
-  attr_accessor :school
-
   def initialize
-    @school = {}
+    @roster = {}
   end
 
-
   def add(name, grade)
-    if @school.keys.include?(grade)
-      @school[grade] << name
-    else
-      @school[grade] = [name]
-    end
+    @roster[grade] = [] unless @roster[grade]
+    (@roster[grade] << name).sort!
   end
 
   def to_h
-    alpha = @school.sort_by { |k,v| v.sort!}.to_h
-    alpha.sort.to_h
+    @roster.sort.to_h
   end
 
   def grade(num)
-    @school[num] ? @school[num] : []
+    @roster[num] || []
   end
-
-
-
 end
-
-scho = School.new
-scho.add('Kelley', 3)
-scho.add('Dan', 3)
-scho.add("Bill", 3)
-p scho.to_h
